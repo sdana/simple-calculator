a54a691d6bed9102de9bfc00b9a555780f5f17f2
sdana <seth.dana@gmail.com>
2018-05-23 13:28:31 -0500

    refactored all math operations into one function

--- main.js
+++ main.js
@@ -7,57 +7,27 @@ let resultDiv = document.getElementById("result")
 let clearButton = document.getElementById("clear")
 
 //Add event listeners for buttons
-addButton.addEventListener("click", addFunc)
-subButton.addEventListener("click", subFunc)
-multButton.addEventListener("click", multFunc)
-divButton.addEventListener("click", divFunc)
+addButton.addEventListener("click", function(){doMath("+")})
+subButton.addEventListener("click", function(){doMath("-")})
+multButton.addEventListener("click", function(){doMath("*")})
+divButton.addEventListener("click", function(){doMath("/")})
 clearButton.addEventListener("click", clearResult)
 
-function addFunc() {
+//function to perform math on inputs
+function doMath(operator) {
     //Grab the values from both text fields
-    let firstNumber = parseInt(document.querySelector("#firstInput").value)
-    let secondNumber = parseInt(document.querySelector("#secondInput").value)
-    
-    //create the object with the result
-    let result = document.createTextNode(`${firstNumber+secondNumber}`)
+    let firstNumber = document.querySelector("#firstInput").value
+    let secondNumber = document.querySelector("#secondInput").value
+
+    //concatenate the string for the operation
+    let operation = firstNumber + operator + secondNumber
+    //perform math operation on string and create DOM node
+    let result = document.createTextNode(`${eval(String(operation))}`)
     let resultH1 = document.createElement("H1")
     resultDiv.appendChild(resultH1).appendChild(result)
 }
 
-function subFunc() {
-    //Grab the values from both text fields
-    let firstNumber = parseInt(document.querySelector("#firstInput").value)
-    let secondNumber = parseInt(document.querySelector("#secondInput").value)
-
-    //create the object with the result
-    let result = document.createTextNode(`${firstNumber - secondNumber}`)
-    let resultH1 = document.createElement("H1")
-    resultDiv.appendChild(resultH1).appendChild(result)
-}
-
-function multFunc() {
-    //Grab the values from both text fields
-    let firstNumber = parseInt(document.querySelector("#firstInput").value)
-    let secondNumber = parseInt(document.querySelector("#secondInput").value)
-
-    //create the object with the result
-    let result = document.createTextNode(`${firstNumber * secondNumber}`)
-    let resultH1 = document.createElement("H1")
-    resultDiv.appendChild(resultH1).appendChild(result)
-}
-
-function divFunc() {
-    //Grab the values from both text fields
-
-    let firstNumber = parseInt(document.querySelector("#firstInput").value)
-    let secondNumber = parseInt(document.querySelector("#secondInput").value)
-
-    //create the object with the result
-    let result = document.createTextNode(`${firstNumber / secondNumber}`)
-    let resultH1 = document.createElement("H1")
-    resultDiv.appendChild(resultH1).appendChild(result)
-}
-
+//function to clear result object from DOM and clear input fields
 function clearResult() {
     //remove result object from DOM
     resultDiv.removeChild(resultDiv.firstChild)
